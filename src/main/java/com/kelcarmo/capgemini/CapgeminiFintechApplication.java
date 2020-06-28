1394d185c951ee4da02bc259ef32796d965edbd3
Kelvin Carmo <kelvin.ocb@gmail.com>
2020-06-28 00:44:50 -0300

    first commit

---
+++ src/main/java/com/kelcarmo/capgemini/CapgeminiFintechApplication.java
@@ -0,0 +1,13 @@
+package com.kelcarmo.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiFintechApplication.class, args);
+	}
+
+}
